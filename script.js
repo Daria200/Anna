9522c65c7c98302169ec86d82caea75aec0f456d
Daria Neznaeva <darianeznaeva@gmail.com>
2021-10-31 11:58:53 -0600

    adding instagram

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ const mainphoto = document.querySelector('.main-photo');
 const headerText = document.querySelector('.header-text');
 const button1 = document.querySelector('.button-1');
 
+
 const tl = new TimelineMax();
 tl.fromTo(mainphoto,1,{x:"-100%"},{x:"0%", ease:Power2.easeInOut})
 .fromTo(headerText,0.5,{opacity:0, x:"30"},{opacity:1, x:"0", ease:Power2.easeInOut}, "-=0.5")
 .fromTo(button1,0.5,{opacity:0, x:"30"},{opacity:1, x:"0", ease:Power2.easeInOut}, "-=0.5")
+
