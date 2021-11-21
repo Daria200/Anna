8adb6a4de858df9bc6b0022390f8406e59bdbbc2
Daria Neznaeva <darianeznaeva@gmail.com>
2021-11-21 12:23:55 -0600

    deleted print

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
-    console.log('click')
   }) 
 
 const mainphoto = document.querySelector('.main-photo');
